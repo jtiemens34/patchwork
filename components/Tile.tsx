c88211da56a731e401096789c1dc5ec949562cd5
elegantlytragic <zomg.its.jesse@hotmail.com>
2023-12-27 20:21:56 -0800

    HoverCard implemented for Tile component details.

--- components/Tile.tsx
+++ components/Tile.tsx
@@ -1,5 +1,13 @@
+import Image from "next/image";
+import buttonIcon from "@/data/images/button.png";
 import EmptyTile from "./empty-tile";
 import FilledTile from "./filled-tile";
+import {
+  HoverCard,
+  HoverCardContent,
+  HoverCardTrigger
+} from "@/components/ui/hover-card";
+import { Hourglass } from "lucide-react";
 
 interface TileProps {
   tileData: {
@@ -14,18 +22,48 @@ const TileComponent = (props: TileProps) => {
   const { buttonCost, timeValue, income, shape } = props.tileData;
   return ( 
     <div>
-      <p><span>Button Cost:</span> {buttonCost}</p>
-      <p><span>Time Value:</span> {timeValue}</p>
-      <p><span>Income:</span> {income}</p>
-      <div>
-        {shape.map((line) => (
-          <div className="max-h-[40px]">
-            {line.map((tile) => (
-              tile == 0 ? <EmptyTile /> : <FilledTile />
+      <HoverCard openDelay={0}>
+        <HoverCardTrigger asChild>
+          <div>
+            {shape.map((line) => (
+              <div className="max-h-[40px]">
+                {line.map((tile) => (
+                  tile == 0 ? <EmptyTile /> : <FilledTile />
+                ))}
+              </div>
             ))}
           </div>
-        ))}
-      </div>
+        </HoverCardTrigger>
+        <HoverCardContent
+          sideOffset={5}
+        >
+          <div>
+            <p>
+              <Image 
+                src={buttonIcon}
+                alt=""
+                width={25}
+                height={25}
+                className="inline mr-1"
+              />
+              Cost: {buttonCost}
+            </p>
+            <p>
+              <Image 
+                src={buttonIcon}
+                alt=""
+                width={25}
+                height={25}
+                className="inline mr-1"
+              />
+              Income: {income}</p>
+            <p>
+              <Hourglass className="inline" />
+              :{timeValue}
+            </p>
+          </div>
+        </HoverCardContent>
+      </HoverCard>
     </div>
   );
 }
