fe630d70dd11fea74ab2b84f63928bb4543fc93e
elegantlytragic <zomg.its.jesse@hotmail.com>
2023-12-27 19:48:01 -0800

    Tile added to index for testing.

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,24 @@
-import Image from 'next/image'
+"use client";
+
+import TileComponent from "@/components/Tile";
+import { Button } from "@/components/ui/button";
+import tiles from "@/data/tiles.json";
+import { useState } from "react";
 
 export default function Home() {
+  const [tile, setTile] = useState(tiles[0]);
+  const newTile = () => {
+    var randomTile = Math.floor(Math.random() * tiles.length);
+    setTile(tiles[randomTile]);
+  };
   return (
-    <p>Hello</p>
+    <>
+      <TileComponent 
+        tileData={tile}
+      />
+      <Button onClick={newTile}>
+        Generate new tile
+      </Button>
+    </>
   );
 }
